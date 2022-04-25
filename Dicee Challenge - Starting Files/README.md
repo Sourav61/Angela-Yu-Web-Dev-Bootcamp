1d0468b25a4cc8adfeb30efba2d5a029c118528f
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-26 00:50:23 +0530

    updated dice challenge solution
    updated dice challenge solution

---
+++ Dicee Challenge - Starting Files/README.md
@@ -0,0 +1,3 @@
+# Dice-Game
+## This Project has been made as a part of Angela Yu Course Section-11 to practice basics of JS
+### You can checkout the deployed version on Heroku here: <em>https://dicegamesp.herokuapp.com/</em>
--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,15 +1,17 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
-document.querySelector(".img1").src = "file:///D:/Desktop/coding/WebDev%20Angela/Angela-Yu-Web-Dev-Bootcamp/Dicee%20Challenge%20-%20Starting%20Files/images/dice" + randomNumber1 + ".png"
+document.querySelector(".img1").src = "https://raw.githubusercontent.com/Sourav61/Dice-Game/main/images/dice" + randomNumber1 + ".png"
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber2);
-document.querySelector(".img2").src = "file:///D:/Desktop/coding/WebDev%20Angela/Angela-Yu-Web-Dev-Bootcamp/Dicee%20Challenge%20-%20Starting%20Files/images/dice" + randomNumber2 + ".png"
+document.querySelector(".img2").src = "https://raw.githubusercontent.com/Sourav61/Dice-Game/main/images/dice" + randomNumber2 + ".png"
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 Won!";
+  document.querySelector("h1").classList.add("small");
 } else if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 2 Won!";
+  document.querySelector("h1").classList.add("small");
 } else {
   document.querySelector("h1").innerHTML = "Ohh! That's a draw!!";
   document.querySelector("h1").classList.add("small");
