9537766788841c52cca6c066ed04e9f41b3931d6
Sourav Pahwa <Sourav61pahwa@gmail.com>
2022-04-28 12:37:04 +0530

    covered till video 7 section 13
    covered till video 7 section 13

---
+++ jQuery/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("h1").addClass("big-heading margin-50");
+})
+
+console.log($("img").attr("src"));
+
+// $("a").attr("href", "https://www.yahoo.com")
+
+// $("h1").css("color","red");
