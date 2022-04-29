396157bb46bd06026df922e99ab34159a07d7ba4
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2022-04-29 11:04:19 +0530

    Covered till Section 13

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,5 +1,28 @@
-$(document).ready(function(){
-    $("h1").addClass("big-heading margin-50");
+// $(document).ready(function(){
+//     $("h1").addClass("big-heading margin-50");
+// })
+
+// for(var i=0;i<5;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color="red";
+//     })
+// }
+
+// $("button").click(function() {
+//     $("h1").css("color","purple");
+// })
+
+// $(document).keydown(function(event){
+//     console.log("event",event)
+//     $("h1").text(event.key);
+// })
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color","purple");
+// })
+
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({ opacity: '0.5' });
 })
 
 console.log($("img").attr("src"));
