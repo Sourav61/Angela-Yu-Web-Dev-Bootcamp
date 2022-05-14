f427ed3ce3c74596328f4d033a5d400308e5ebd8
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-14 20:23:44 +0530

    Covered Till Video 9 Section 21
    Covered Till Video 9 Section 21

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,39 +3,72 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function (req, res) {
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     })
 });
 
 app.post("/", function (req, res) {
-    var item = req.body.newItem
-    console.log(item);
-    items.push(item);
+    let item = req.body.newItem;
+    console.log("re", req.body)
 
+    if (req.body.list == "Work List") {
+        if (item != "") {
+            workItems.push(item);
+        } else {
+            console.log("alert")
+        }
+        res.redirect("/work");
+    } else {
+        if (item != "") {
+            console.log(item);
+            items.push(item);
+
+        } else {
+            console.log("alert")
+        }
+        res.redirect("/");
+    }
     // res.render("list", {
     //     newItem: item
     // })
+})
 
-    res.redirect("/");
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    })
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function (req, res) {
+    res.render("about");
 })
 
 app.listen(3004, function () {
