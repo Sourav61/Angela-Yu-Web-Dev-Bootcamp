4d5cd52af16d0730afdeafd3647740c1094c25ff
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-14 16:38:17 +0530

    Covered Till Video Lecture 6 Section 21
    Covered Till Video Lecture 6 Section 21

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,41 +3,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", function (req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log("Error: The current day is: ", currentDay)
-            break;
-    }
 
-    res.render("list", { kindOfDay: day })
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    })
 });
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem
+    console.log(item);
+    items.push(item);
+
+    // res.render("list", {
+    //     newItem: item
+    // })
+
+    res.redirect("/");
+})
 
 app.listen(3004, function () {
     console.log("Server running on port 3004");
