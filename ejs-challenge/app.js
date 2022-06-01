b8f471f8aa57e59a7afd57d3e86aecce3972aa67
sourav pahwa <Sourav61pahwa@gmail.com>
2022-06-01 22:52:56 +0530

    Completed Section 22
    Completed Section 22

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -54,9 +55,22 @@ app.post("/compose", function(req,res){
   res.redirect("/");
 })
 
+app.get("/posts/:post", function(req,res) {
 
+  const requestedTitle = _.lowerCase(req.params.post);
+  const content = req.params.content;
 
-
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    const storedContent = post.content;  
+    if(storedTitle === requestedTitle){
+      res.render("post",{
+        title: post.title,
+        content: storedContent
+      })
+    }
+  })
+})
 
 app.listen(3005, function () {
   console.log("Server started on port 3005");
