347555cc9b5e4692df30f22605980cf848401360
sourav pahwa <Sourav61pahwa@gmail.com>
2022-06-07 18:49:28 +0530

    Completed Section 26
    Completed Section 26

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -57,63 +57,147 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/fruitsDB');
+    await mongoose.connect('mongodb://localhost:27017/fruitsDB');
 }
 
 const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: [true, "Please check your data entry, no name specified!"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
+// const fruit = new Fruit({
+//     rating: 10,
+//     review: "Peaches are so yummy!"
+// })
+
 const fruit = new Fruit({
     name: "Apple",
-    rating: 8,
-    review: "An apple day keeps a doctor away!!"
+    rating: 9,
+    review: "An apple a day keeps the doctor away!"
 })
 
 // fruit.save();
 
 const personSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    favouriteFruit: fruitSchema
 });
 
-const Person = mongoose.model("Person",personSchema);
+const Person = mongoose.model("Person", personSchema);
+
+const pineapple = new Fruit({
+        name: "Pineapple",
+        score: 9,
+        review: "Great Fruit."
+});
+
+// pineapple.save();
 
 const person = new Person(
     {
-        name: "Sourav",
-        age: 18
+        name: "John Doe",
+        age: 37,
+        favouriteFruit: pineapple
     }
 )
 
-person.save();
-
-const kiwi = new Fruit({
-    name: "Kiwi",
-    score: 3,
-    review: "The eww fruit"
+const Sour = new Person({
+    name: "Sourav",
+    age: 18
 })
 
-const orange = new Fruit({
-    name: "Orange",
-    score: "5",
-    review: "The sour fruit"
+// person.save();
+
+// Sour.save();
+
+// const kiwi = new Fruit({
+//     name: "Kiwi",
+//     score: 3,
+//     review: "The eww fruit"
+// })
+
+// const orange = new Fruit({
+//     name: "Orange",
+//     score: "5",
+//     review: "The sour fruit"
+// })
+
+// const banana = new Fruit({
+//     name: "Banana",
+//     score: 7,
+//     review: "Tasty fruit"
+// })
+
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Successfully saved all fruits to FruitsDB");
+//     }
+// });
+
+// Fruit.updateOne({ _id: "629f18b09351c2d4072002f0" }, { name: "Peach" }, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully updated the content")
+//     }
+// })
+
+// Fruit.deleteOne({ name: "Peach" }, function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Successfully deleted the document!")
+//     }
+// })
+
+Fruit.find(function (err, fruits) {
+    if (err) {
+        console.log(err);
+    } else {
+        // console.log(fruits);
+
+        // mongoose.connection.close();
+
+        fruits.forEach(function (fruit) {
+            console.log(fruit.name);
+        })
+
+    }
 })
 
-const banana = new Fruit({
-    name: "Banana",
-    score: 7,
-    review: "Tasty fruit"
-})
+// Person.deleteMany({ name: "Sourav" }, function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Succefully deleted all of the documents");
+//     }
+// })
 
-Fruit.insertMany([kiwi, orange, banana], function(err){
+// Fruit.deleteMany({ name: "Apple" },function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Successfully deleted the document!");
+//     }
+// })
+
+Person.updateOne({ name: "Sourav" }, { favouriteFruit: fruit }, function(err){
     if(err){
         console.log(err);
     }else{
-        console.log("Successfully saved all fruits to FruitsDB");
+        console.log("Successfuly added relationship of person with fruit document");
     }
-});
+})
